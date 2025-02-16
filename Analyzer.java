6d362892027bf1de055322a963d9e01bc16b14b8
wilsonlin-stonybrook <wilson.lin.2@stonybrook.edu>
2025-02-16 18:08:23 -0500

    Update Analyzer.java

--- Analyzer.java
+++ Analyzer.java
@@ -2,14 +2,6 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
- * Wilson Lin
- * 115091711
- * wilson.lin.2@stonybrook.edu
- * Hw 3
- * CSE214.R04 Summer 2024
- */
-
-/**
  * The <code>Analyzer</code> class will be used to take user inputs and
  * simulate the simulator class.
  */
