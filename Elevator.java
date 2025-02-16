bf46c9233c6f3afd79cc2b028bfac4b08d57a157
wilsonlin-stonybrook <wilson.lin.2@stonybrook.edu>
2025-02-16 18:08:36 -0500

    Update Elevator.java

--- Elevator.java
+++ Elevator.java
@@ -1,10 +1,3 @@
-/**
- * Wilson Lin
- * 115091711
- * wilson.lin.2@stonybrook.edu
- * Hw 3
- * CSE214.R04 Summer 2024
- */
 
 /**
  * The <code>Elevator</code> class is used for the creation of an elevator
